8277e9ecb56bd26f19f8aa97d947fbe5e9d86781
Pugachev85 <workpost2008@mail.ru>
2022-08-14 23:17:13 +0300

    Chapter 2 add 3

--- src/main/java/com/IvanGP/Taco/Taco.java
+++ src/main/java/com/IvanGP/Taco/Taco.java
@@ -1,11 +1,19 @@
 package com.IvanGP.Taco;
 
 import lombok.Data;
+import lombok.NonNull;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min=5, message="Name must be at last 5 characters long")
     private String name;
+
+    @NotNull
+    @Size(min=1, message="You must choose at last 1 ingredient")
     private List<Ingredient> ingredients;
 }
